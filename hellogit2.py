45dbb6863a0a19f5d0755002176d63840adc2c42
AtardecerR0j0 <casewhenloop@gmail.com>
2026-02-25 00:35:19 +0100

    Duplicate print statements and add conflict message

--- hellogit2.py
+++ hellogit2.py
@@ -2,4 +2,6 @@ print("Hello git 2 - modificadolos")
 print("Existe otra rama paralela que se llama contingencia")
 print("Quiero dejar otro WIP en la rama contingencia")
 print("A ver si creo conflicto en GitHub")
+print("Modifico en editor GitHub y ahora conflicto por mis putos huevos")
 print("Hello GitHub")
+
